f86baec64cfa6ca1f3fc9f015417031f2a9780bd
GatoSeries <121359333+GatoSeries@users.noreply.github.com>
2023-07-13 13:44:24 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,31 @@
-function preload(){
+function preload() {
 
 }
-function setup(){
+function setup() {
     canvas = createCanvas(350, 350);
     canvas.center();
+    video = createCapture(VIDEO)
+    video.size(350, 350);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoad);
+    poseNet.on("pose", poses);
 }
-function draw(){
+function draw() {
+    image(video, 0, 0, 350, 350);
 
+
+}
+function modelLoad() {
+    console.log("POSENET LOADED");
+
+}
+function poses(results) {
+
+    if (results.length > 0) {
+        console.log(results);
+        console.log("x, " + results[0].pose.nose.x);
+        console.log("y, " + results[0].pose.nose.y);
+        console.log(results[0].pose.nose);
+
+    }
 }
